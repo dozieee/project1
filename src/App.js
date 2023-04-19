304a9d81c1390de5d90583636073b805a260e66e
Chimezie Favour <Doziefavour20@gmail.com>
2023-04-19 14:55:20 +0100

    fix: React framework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './Components/greet';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+        <p class="text-3xl bg-blue-400 font-bold">
+    Hello world!
+  </p>
+  <MyComponent  />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Components/greet.js
@@ -0,0 +1,8 @@
+import  React  from 'react';
+
+function greet() {
+    return <h1>
+        Hello, Jack You are a star</h1>
+}
+
+export default greet
